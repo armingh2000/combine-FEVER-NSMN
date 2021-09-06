0fbe0213152d717765ec2ac3d9a081c8e659a634
armingh2000 <armingh379@gmail.com>
2021-09-06 20:06:12 +0430

    remove total

--- src/train/train.py
+++ src/train/train.py
@@ -96,7 +96,7 @@ def train_nn_doc(model_name):
         sampled_train_instances = train_fever_data_reader.read(complete_upstream_train_data)
 
         train_iter = biterator(sampled_train_instances, shuffle=True, num_epochs=1, cuda_device=device_num)
-        for i, batch in tqdm(enumerate(train_iter), desc=f"epoch: {i_epoch} / {num_epoch}, iteration: ", total=len(complete_upstream_train_data)):
+        for i, batch in tqdm(enumerate(train_iter), desc=f"epoch: {i_epoch} / {num_epoch}, iteration: "):
             model.train()
             out = model(batch)
             y = batch['selection_label']
